64e1dc5f7d6fd4c2544da6d898db514bfe4aea6e
Teja Srinivas Raj Maganti <tejasrinivasraj.m@thoughtworks.com>
2020-02-08 15:19:14 +0530

    Req1.2:|Add. Biblioteca displays a single book if there is only one Book|

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -5,9 +5,14 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
         bibliotecaApp.displayMessage();
+        bibliotecaApp.displayBooks();
     }
 
     private void displayMessage() {
         System.out.println(Constants.welcomeMessage);
+    }
+
+    private void displayBooks() {
+        System.out.println("TDD By Example");
     }
 }
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -3,6 +3,9 @@ package com.twu.biblioteca;
 import org.junit.jupiter.api.Test;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -15,5 +18,14 @@ class BibliotecaAppTest {
         BibliotecaApp.main(new String[]{});
 
         verify(out).println(Constants.welcomeMessage);
+    }
+
+    @Test
+    void shouldDisplayOneBooksIfThereIsOnlyOneBook() {
+        PrintStream out = mock(PrintStream.class);
+        System.setOut(out);
+        BibliotecaApp.main(new String[]{});
+
+        verify(out).println("TDD By Example");
     }
 }
