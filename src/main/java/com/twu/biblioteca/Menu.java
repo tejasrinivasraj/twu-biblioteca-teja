627c57fcaa2a7194e1dbf0f0f7879a3ebfc13bac
Teja Srinivas Raj Maganti <tejasrinivasraj.m@thoughtworks.com>
2020-02-09 15:30:34 +0530

    Req1.6:|Add. Menu displays one option if there is only one|

---
+++ src/main/java/com/twu/biblioteca/Menu.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+class Menu {
+    void displayOptions() {
+        System.out.println(Constants.LIST_OF_BOOKS);
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/MenuTest.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class MenuTest {
+
+    @Test
+    void shouldDisplayOneOptionInMenuIfThereIsOnlyOne() {
+        PrintStream out = mock(PrintStream.class);
+        System.setOut(out);
+        Menu menu = new Menu();
+
+        menu.displayOptions();
+
+        verify(out).println(Constants.LIST_OF_BOOKS);
+    }
+}
