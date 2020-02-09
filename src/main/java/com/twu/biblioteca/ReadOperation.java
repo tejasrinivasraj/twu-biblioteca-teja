87ff011edef6b4390ad7c23e8aafc7292b931ef5
Teja Srinivas Raj Maganti <tejasrinivasraj.m@thoughtworks.com>
2020-02-09 15:16:50 +0530

    |Refactor. Read Operation State|

--- src/main/java/com/twu/biblioteca/ReadOperation.java
+++ src/main/java/com/twu/biblioteca/ReadOperation.java
@@ -3,7 +3,7 @@ package com.twu.biblioteca;
 import java.util.Scanner;
 
 class ReadOperation {
-    Scanner scan = new Scanner(System.in);
+    private Scanner scan = new Scanner(System.in);
 
     String userChoice() {
         return scan.next();
